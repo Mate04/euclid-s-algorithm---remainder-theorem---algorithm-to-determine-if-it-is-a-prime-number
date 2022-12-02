9597b093df7f0caeec31917b86512447d4872c82
Mate04 <matusmaldonado@hotmail.com>
2022-12-02 19:03:40 -0300

    1.1.1

--- algorithms.py
+++ algorithms.py
@@ -1,3 +1,17 @@
+import math
+
+def largestNumber1(num:int):
+    while num < 1:
+        num = int(input('Please, choose a number greater than 0: '))
+    return num
+def largestNumber2(num:int):
+    while num <= 1:
+        num = int(input('Please, choose a number greater than 1: '))
+    return num
+def checkAgreaterB(a:int,b:int):
+    while a < b:
+        b = int(input(f'Please, choose a number less than {a}: '))
+    return a,b
 def euclideanAlgorithm(a:int,b:int):
     a, b = checkAgreaterB(a,b)
     AuxA, AuxB = a,b
@@ -20,11 +34,15 @@ def remainderTheorem(a:int,b:int):
         c = a//b
         r = a - (c*b)
     print(f'el MCD({AuxA},{AuxB}) es {b}')
-def largestNumber1(num:int):
-    while num < 1:
-        num = int(input('Please, choose a number greater than 0: '))
-    return num
-def checkAgreaterB(a:int,b:int):
-    while a < b:
-        b = int(input(f'Please, choose a number less than {a}: '))
-    return a,b
+
+def aPrimeNumber(num):
+    k = int(math.sqrt(num))
+    if num == 2:
+        return False
+    elif num % 2 == 0:
+        return False
+    while k != 1:
+        if num % k == 0:
+            return False
+        k -= 1
+    return True
--- main.py
+++ main.py
@@ -6,5 +6,11 @@ def main():
             euclideanAlgorithm(largestNumber1(int(input('First element: '))),largestNumber1(int(input('Seconds element: '))))
         elif Option == 2:
             remainderTheorem(largestNumber1(int(input('First element: '))),largestNumber1(int(input('Seconds element: '))))
-        Option = int(input('Option 1: MCD between two numbers\nOption 2: MCD applying remainder theorem\nOption 0: Out\nOption: '))
+        elif Option == 3:
+            number =  largestNumber2(int(input('what number do you want to check if it is prime? ')))
+            if not aPrimeNumber(number):
+                print(f"The number {number} isn't prime")
+            else:
+                print(f"The number {number} is prime")
+        Option = int(input('Option 1: MCD between two numbers\nOption 2: MCD applying remainder theorem\nOption 3: check if it is a prime number\nOption 0: Out\nOption: '))
 main()
